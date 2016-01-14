71ee65b1e6399c5365dad782d18cc207c3e6b463
Andy Palka <andrewmpalka@gmail.com>
2016-01-14 15:21:40 -0600

    added method

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -29,9 +29,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(IBAction)onButtonTapped:(UIButton *)sender {
 }
+
 
 @end
